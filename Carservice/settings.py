1ad7bad19c83f93f3c7520c8c27385b63a356603
GrishinSV <svg95@list.ru>
2024-05-26 17:54:16 +0300

    full

--- Carservice/settings.py
+++ Carservice/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +78,19 @@ WSGI_APPLICATION = 'Carservice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgres://carservice_ewv8_user:ji7XF0mcNZtPmAcOCGxZmVNEDRz7Yrbn@dpg-cp9knpkf7o1s73a2gfd0-a/carservice_ewv8',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # Password validation
